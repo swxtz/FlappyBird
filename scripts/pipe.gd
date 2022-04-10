2f85398ae1bcb47296b7f7b06f9ec8d1f296866d
swxtz <82668127+swxtz@users.noreply.github.com>
2022-04-10 18:28:20 -0300

    10/04

---
+++ scripts/pipe.gd
@@ -0,0 +1,27 @@
+extends Node2D
+class_name Pipe
+
+onready var top_pipe: StaticBody2D = get_node("TopPipe")
+onready var bottom_pipe: StaticBody2D = get_node("BottomPipe")
+
+export(int) var walk_speed 
+export(Array, String) var pipe_texture
+
+func _ready() -> void:
+	randomize()
+	var random_number: int = randi() % pipe_texture.size()
+	top_pipe.get_node("Texture").texture = load(pipe_texture[random_number])
+	bottom_pipe.get_node("Texture").texture = load(pipe_texture[random_number])
+
+
+func _physics_process(delta : float) -> void:
+	position.x -= delta * walk_speed
+
+func on_screen_exited()-> void:
+	queue_free()
+	
+
+
+func on_body_entered(body: Bird)-> void:
+	if body is Bird and body.global.position.x > get_node("ScoreArea/Collision").global_position.x: 
+		get_tree().call_group("Interface", "update_score")
